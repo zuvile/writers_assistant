dd8b014f854ecf81142c2e8e21aec169ab95fcca
zuvile <zivile.mitke@gmail.com>
2024-05-19 10:54:59 +0300

    fix typo

--- writers_assistant/asgi.py
+++ writers_assistant/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for writers_assitant project.
+ASGI config for writers_assistant project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'writers_assitant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'writers_assistant.settings')
 
 application = get_asgi_application()
--- writers_assistant/wsgi.py
+++ writers_assistant/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for writers_assitant project.
+WSGI config for writers_assistant project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'writers_assitant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'writers_assistant.settings')
 
 application = get_wsgi_application()
